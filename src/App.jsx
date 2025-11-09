926382488c71d38e88e2fdc094430836323c020a
Tara <taraparhamrad@gmail.com>
2025-11-09 13:11:00 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-gray-100 text-2xl font-semibold">
+      Dashbord Project
+    </section>
+  );
+}
+export default App;
