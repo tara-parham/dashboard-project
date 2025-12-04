cf11e1e2f002fa180266d6bb03c0538dc2ba44f6
Tara <taraparhamrad@gmail.com>
2025-12-04 18:22:06 +0330

    added localStorage

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,14 +1,26 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { products as initialProducts } from "../data/products";
 import ProductReducer from "./ProductReducer";
 
 export const ProductContext = createContext();
 
 export default function ProductProvider({ children }) {
-  const [products, dispatch] = useReducer(
-    ProductReducer,
-    initialProducts.map((p) => ({ ...p, count: 0 }))
-  );
+  //function
+  const init = () => {
+    const saved = localStorage.getItem("products");
+    if (saved) return JSON.parse(saved);
+    return initialProducts.map((p) => ({ ...p, count: 0 }));
+  };
+
+  //usereducer
+  const [products, dispatch] = useReducer(ProductReducer, [], init);
+
+  //useEffect
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(products));
+  }, [products]);
+
+  //return
   return (
     <ProductContext.Provider value={{ products, dispatch }}>
       {children}
